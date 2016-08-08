e80ea6778ef45bdb7452c43d73f27aafb95a7e05
Garan Guillory <garanguillory@gmail.com>
2016-08-08 13:16:04 -0500

    Configuring react promise middleware, setting up actions and reducers for posts, and set up IndexRoute.

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React Redux Simple Blog</div>
+      <div>
+      	{this.props.children}
+      </div>
     );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 	// the Route object is what we are going to use 
 	// to define a match between a url and a component
 import App from './components/app';
-
+import PostsIndex from './components/posts_index';
 
 
 export default (
-	<Route path="/" component={App} />
-	// whenever the user is at this path "/" show this component 
+		// whenever the user is at this path "/" show this component 
+	<Route path="/" component={App}>
+		<IndexRoute component={PostsIndex} />
+	</Route>
 );
